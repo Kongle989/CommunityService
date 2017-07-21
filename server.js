80f181805ecd6888605afb0dd39e1c66568597d7
Kong Le <kongle989@yahoo.com>
2017-07-21 13:40:04 -0400

    server set up, db set up

--- server.js
+++ server.js
@@ -3,4 +3,24 @@ let express = require("express"),
     logger = require("morgan"),
     mongoose = require("mongoose"),
     app = express(),
-    url = 'mongodb://localhost/nytreact';
+    url = 'mongodb://localhost/helpinghand';
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+mongoose.Promise = Promise;
+app.use(express.static("public"));
+
+mongoose.connect(url);
+
+let db = mongoose.connection;
+db.on("error", function (error) {
+    console.log("Mongoose Error: ", error);
+});
+db.once("open", () => {
+    console.log("Mongoose connection successful.");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("App running");
+});
